43d96d32c9811a30412c43d83dd6e85185373bf9
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2017-09-29 11:46:51 +0200

    Added tests.

--- springloggenerator/src/test/java/es/loganalyzer/springloggenerator/PasswordGeneratorTest.java
+++ springloggenerator/src/test/java/es/loganalyzer/springloggenerator/PasswordGeneratorTest.java
@@ -1,16 +1,60 @@
 package es.loganalyzer.springloggenerator;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class PasswordGeneratorTest {
 
 	@Test
-	public void regionalCharsTest(){
-		PasswordGenerator tester = new PasswordGenerator(new Index(), new String[]{"2", "Medium", "false", "true", "false", "US / UK"});
+	public void checkRegionalChars(){
+		PasswordGenerator tester = new PasswordGenerator(new Index(), new String[]{"12", "Medium", "false", "true", "false", "US / UK"});
+		PasswordGenerator tester2 = new PasswordGenerator(new Index(), new String[]{"8", "Advanced", "true", "true", "true", "SP"});
 		
-		assertEquals("regionChars must contain US / UK regional chars.", 0, tester.regionChars.size());
+		assertEquals("tester;regionChars must contain US / UK regional chars.", 0, tester.regionChars.size());
+		assertEquals("tester2;regionChars must contain SP regional chars.", 2, tester2.regionChars.size());
+	}
+	
+	@Test
+	public void checkUpdateValues(){
+		PasswordGenerator tester = new PasswordGenerator(new Index(), new String[]{"12", "Medium", "false", "true", "false", "US / UK"});
+		PasswordGenerator tester2 = new PasswordGenerator(new Index(), new String[]{"8", "Advanced", "true", "true", "true", "SP"});
+		
+		assertNotNull("tester;first condition;numChat must contain a valid int value.", tester.numChar);
+		assertNotNull("tester;second condition;secLevel must contain a valid String value.", tester.secLevel);
+		assertNotNull("tester;third condition;specialChar must contain a valid boolean value.", tester.specialChar);
+		assertNotNull("tester;fourth condition;numbers must contain a valid boolean value.", tester.numbers);
+		assertNotNull("tester;fifth condition;wantRepeated must contain a valid boolean value.", tester.wantRepeated);
+		assertNotNull("tester;fifth condition;regionChar must contain a valid String value.", tester.regionChar);
+		
+		assertNotNull("tester2;first condition;numChat must contain a valid int value.", tester2.numChar);
+		assertNotNull("tester2;second condition;secLevel must contain a valid String value.", tester2.secLevel);
+		assertNotNull("tester2;third condition;specialChar must contain a valid boolean value.", tester2.specialChar);
+		assertNotNull("tester2;fourth condition;numbers must contain a valid boolean value.", tester2.numbers);
+		assertNotNull("tester2;fifth condition;wantRepeated must contain a valid boolean value.", tester2.wantRepeated);
+		assertNotNull("tester2;fifth condition;regionChar must contain a valid String value.", tester2.regionChar);
+	}
+	
+	@Test
+	public void checkSecLevel(){
+		PasswordGenerator tester = new PasswordGenerator(new Index(), new String[]{"12", "Medium", "false", "true", "false", "US / UK"});
+		PasswordGenerator tester2 = new PasswordGenerator(new Index(), new String[]{"8", "Advanced", "true", "true", "true", "SP"});
+
+		assertNotNull("tester;secLevel must have an int value in gradeLevel variable.", tester.gradeLevel);
+		assertNotNull("tester2;secLevel must have an int value in gradeLevel variable.", tester2.gradeLevel);
+		
+	}
+	
+	@Test
+	public void checkCreatePassword(){
+		PasswordGenerator tester = new PasswordGenerator(new Index(), new String[]{"12", "Medium", "false", "true", "false", "US / UK"});
+		PasswordGenerator tester2 = new PasswordGenerator(new Index(), new String[]{"8", "Advanced", "true", "true", "true", "SP"});
+		
+		String password = tester.createPassword();
+		
+		assertTrue("Password generated and its value is " + password, true);
 	}
 	
 }
